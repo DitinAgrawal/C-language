7f6cf3538a6979c9e295d891cc2041b8c0588c7e
Ditin Agrawal <113216643+DitinAgrawal@users.noreply.github.com>
2022-10-12 06:59:49 +0530

    Assignment One

---
+++ Assignment_one.c
@@ -0,0 +1,515 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+// ============== Question - 1 ===============
+
+    // float price,tax,total;
+    // printf("Enter Original Price and Tax Rate Respectively :");
+    // scanf("%f%f",&price,&tax);
+    // tax = (tax / 100) * price;
+    // total = price + tax;
+    // printf("Your Product Price Is : %.2f", total);
+
+
+// ============== Question - 2 ===============
+
+
+    // int wages, hours, rate, extra_hrs, extra_wage;
+    // printf("Enter The Employee Worked Hours & Wage Per Hour Respectively :");
+    // scanf("%d%d",&hours,&rate);
+    // if (hours > 30)
+    // {
+    //     extra_hrs = hours - 30;
+    //     wages = 30 * rate;
+    //     extra_wage = extra_hrs * (2 * rate);
+    //     extra_wage += wages;
+    //     printf("Yore Weekly Wages Is : %d", extra_wage);
+    // }
+    // else{
+    //     wages = hours * rate;
+    //     printf("Your Weekly Wages Is : %d", wages);
+    // }
+
+
+// ============== Question - 3 ===============
+
+
+    // float money,spend;
+    // money = 500;
+    // spend = (2.0 * 50.0) + (1.5 * 35.0) + (2.5 * 10.0) + (1.0 * 15.0);
+    // printf("The Amount Mr. X Get In The Return Is : %.2f", money - spend);
+
+
+
+// ============== Question - 4 ===============
+
+
+    // printf("Name : Ditin Agrawal\n");
+    // printf("DOB : 16 January 2005\n");
+    // printf("Mobile : 8958446414");
+
+
+
+// ============== Question - 5 ===============
+
+
+    // int num;
+    // float deci;
+    // char ch;
+    // printf("Enter a Character : ");
+    // scanf("%c",&ch);
+    // printf("Enter a Number : ");
+    // scanf("%d",&num);
+    // printf("Enter a Decimal : ");
+    // scanf("%f",&deci);
+    // printf("\nYour Character Is : %c\n",ch);
+    // printf("Your Number Is : %d\n",num);
+    // printf("Your decimal Is : %f",deci);
+    
+
+
+
+// ============== Question - 6 ===============
+
+
+    // float cost = 172.53;
+    // printf("The sales total is : $ %g", cost);
+
+
+
+// ============== Question - 7 ===============
+
+
+
+    // float num = 6.5;
+    // printf("Raju Have %g Apples", num*3.0);
+
+
+
+// ============== Question - 8 ===============
+
+
+    // float num;
+    // printf("Enter A number : ");
+    // scanf("%f",&num);
+    // printf("%.2e", num);
+
+
+// ============== Question - 9 ===============
+
+
+    // double num;
+    // printf("Enter Your Mobile Number : ");
+    // scanf("%lf",&num);
+    // printf("Your Mobile Number is : %.0lf", num);
+
+
+
+// ============== Question - 10 ===============
+
+
+    // int now,one_year,after;
+    // now  = 30000;
+    // one_year = 0.2*now;
+    // after = 0.3 * now;
+    // now += one_year + after;
+    // printf("%d", now);
+
+
+
+// ============== Question - 11 ===============
+
+
+    // char letter;
+    // printf("Enter a Letter  : ");
+    // scanf("%c", &letter);
+    // int value = letter;
+    // printf("Your ASCII Value Of Your Character Is : %d",value);
+
+
+
+// ============== Question - 12 ===============
+
+
+    // float salary,hra,ta;
+    // printf("Enter your Basic Salary : ");
+    // scanf("%f", &salary);
+    // hra = 0.15 * salary;
+    // ta = 0.20 * salary;
+    // printf("%.2f",salary + hra + ta);
+
+
+
+// ============== Question - 13 ===============
+
+
+
+    // float coord1,coord2,coord3,coord4,slope,angle;
+    // printf("Enter Coordinates (Like a b x y) : ");
+    // scanf("%f%f%f%f",&coord1,&coord2,&coord3,&coord4);
+    // slope = (coord4 - coord2) / (coord3 - coord1);
+    // printf("Slope Of Your Line Is : %f", slope);
+    // angle = atan(slope) * 180/3.14;
+    // printf("\nAngle Of Your Line Is : %f",angle);
+
+
+
+// ============== Question - 14 ===============
+
+
+
+    // int c1,c2,c3,c4,c5,g1,g2,g3,g4,g5;
+    // printf("Enter Credits and Grades Respectively : ");
+    // scanf("%d%d%d%d%d%d%d%d%d%d",&c1,&c2,&c3,&c4,&c5,&g1,&g2,&g3,&g4,&g5);
+    // float spi = (c1*g1 + c2*g2 + c3*g3 + c4*g4 + c5*g5)/(c1+c2+c3+c4+c5);
+    // printf("Your SPI Is %.3f",spi);
+
+
+
+// ============== Question - 15 ===============
+
+
+    // float speed,wavelength,frequency;
+    // printf("Enter Speed And Wavelength :");
+    // scanf("%f%f", &speed,&wavelength);
+    // frequency = speed / wavelength;
+    // printf("Frequecy Of The Wave Is : %f", frequency);
+
+
+
+// ============== Question - 16 ===============
+
+
+    
+
+
+
+// ============== Question - 18 ===============
+
+
+
+    // int x,rollno,a,b,c,d;
+    // printf("Enter Your Roll Number : ");
+    // scanf("%4d%4d",&x,&rollno);
+    // printf("\nFour Last Digits Of Your Roll Number Is : %d",rollno);
+    // a = rollno / 1000;
+    // b = (rollno % 1000) / 100;
+    // c = ((rollno % 1000) % 100) / 10;
+    // d = ((rollno % 1000) % 100) % 10;
+    // printf("\nSum Of four Last Digits Of Your Roll Number Is : %d",a + b + c + d);
+
+
+
+// ============== Question - 19 ===============
+
+
+    // float w,h;
+    // printf("Enter Your Weight(Kg) and Height(cm) Respectively : ");
+    // scanf("%f%f",&w,&h);
+    // w = w * 2.20462;
+    // h = (h * 0.393701) / 12;
+    // printf("%.2f\n%.2f", w,h);
+
+
+
+// ============== Question - 20 ===============
+    
+
+    // char option;
+    // int sum = 0;
+    // float product = 1;
+
+
+
+// ============== Question - 21 ===============
+
+
+    // int a,b,c,d,e,f,g,h,i;
+    // printf("Enter Any Nine Numbers (Use space To differentiate : )");
+    // scanf("%d%d%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g,&h,&i);
+    // printf("\n%d,%d,%d\n%d,%d,%d\n%d,%d,%d",a,b,c,d,e,f,g,h,i);
+
+
+
+
+// ============== Question - 23 ===============
+
+
+    // int num=070;
+    // printf("%d\t%o\t%x",num,num,num);
+
+
+    /* ******* Solution ********
+
+    ---->    56    70     38
+
+    ******* Solution ******** */
+
+
+
+
+// ============== Question - 24 ===============
+
+
+    // int x = printf("GLA UNIVERSITY");
+   	// printf("%d", x);
+
+
+    /* ******* Solution ********
+
+    ---->    GLA UNIVERSITY14
+
+    ******* Solution ******** */
+
+
+
+
+// ============== Question - 26 ===============
+
+
+    // int x = printf("C is placement oriented Language") - printf("Hi");
+ 	// printf("%d %o %x", x,x,x);
+
+
+    /* ******* Solution ********
+
+    ---->    C is placement oriented LanguageHi30 36 1e
+
+    ******* Solution ******** */
+
+
+
+// ============== Question - 28 ===============
+
+
+
+    // printf(" \"C %% FOR %% PLACEMENT\""); 
+
+    /* ******* Solution ********
+
+    ---->    "C % FOR % PLACEMENT"
+
+    ******* Solution ******** */
+
+    
+
+
+
+// ============== Question - 29 ===============
+
+
+
+    // float distance;
+    // printf("Enter A Distance In Km Only : ");
+    // scanf("%f",&distance);
+    // printf("The Speed Of The Bus Is : %.2f Km/hr", distance/4.0);
+
+
+
+
+
+// ============== Question - 30 ===============
+
+
+
+    // float a,b,c;
+    // printf("Enter the Marks Of First Participants : ");
+    // scanf("%f",&a);
+    // printf("Enter the Marks Of Second Participants : ");
+    // scanf("%f",&b);
+    // printf("Enter the Marks Of Third Participants : ");
+    // scanf("%f",&c);
+    // printf("The Average Of The Marks Of All Three Partcipants is %.2f", (a+b+c) / 3);
+
+
+
+
+// ============== Question - 31 ===============
+
+
+    // int a,b;
+    // printf("How Much Do you want To Give Saurav : ");
+    // scanf("%d",&a);
+    // printf("How Much Do you want To Give Sajal : ");
+    // scanf("%d",&b);
+    // a = a + b;
+    // b = a - b;
+    // a = a - b;
+    // printf("Saurav Have %d Rupees,\nand Sajal Have %d Rupees", a,b);
+
+
+
+
+// ============== Question - 32 ===============
+
+
+
+    // float speed,time;
+    // printf("At What Speed(km/hr) You are Running : ");
+    // scanf("%f",&speed);
+    // printf("How Much Time (min.) Did You Take To Reach Your Mess : ");
+    // scanf("%f",&time);
+    // time = time / 60;
+    // printf("The Distance Travelled By You Is : %.2f Meter", speed * time);
+
+
+
+
+// ============== Question - 36 ===============
+
+
+    // if (sizeof(int) > -1)
+    //     printf("Yes");
+    // else
+    //     printf("No");
+
+
+    /* ******* Solution ********
+
+    ---->    No
+
+    ******* Solution ******** */
+
+
+
+
+// ============== Question - 38 ===============
+
+
+    // float value;
+    // printf("Enter The Volume Of Tank In Gallons : ");
+    // scanf("%f",&value);
+    // printf("Your Required Time Is : %.2f Hours",value / 25.0);
+
+
+
+// ============== Question - 44 ===============
+
+
+    // int x=0;
+    // x= printf("\"hello\b\"");
+    // printf("%d",x);
+
+
+    /* ******* Solution ********
+
+    ---->    "hell"8
+
+    ******* Solution ******** */
+
+
+
+// ============== Question - 45 ===============
+
+
+    // int b,c=5 ;
+    // printf("%d,%d", b,c);
+
+
+    /* ******* Solution ********
+
+    ---->    garbage,5
+
+    ******* Solution ******** */
+
+
+
+
+// ============== Question - 47 ===============
+   
+
+    // char x, a='c';
+    // x=printf("%c",a);
+    // printf("%d", x);
+
+
+     /* ******* Solution ********
+
+    ---->    c1
+
+    ******* Solution ******** */
+
+
+
+// ============== Question - 53 ===============
+
+
+//    int  a = 32770;
+//    printf("%d", a);
+
+
+    /* ******* Solution ********
+
+    ---->    32770
+
+    ******* Solution ******** */
+
+
+
+// ============== Question - 54 ===============
+
+
+//    float c = 5.0;
+//    printf ("Temperature in Fahrenheit is %.2f", (9/5) * c + 32);
+
+
+    /* ******* Solution ********
+
+    ---->    41.00
+
+    ******* Solution ******** */
+
+
+
+
+
+// ============== Question - program to convert a integer (in sec. ) to hours, minutes & Seconds ===============
+
+
+    // int seconds,hours,minutes;
+    // printf("Enter Time in Seconds : ");
+    // scanf("%d", &seconds);
+    // minutes = seconds / 60;
+    // hours = minutes / 60;
+    // printf("%d Hours : %d Minutes : %d Seconds ", hours,minutes,seconds);
+
+
+
+
+// ============== Question - program to convert integer (in days) to years, months and days ===============
+
+
+    // int days,months,years;
+    // printf("Enter Numbers Of Days : ");
+    // scanf("%d", &days);
+    // months = days / 30;
+    // years = months / 12;
+    // // years = days / 365;
+    // printf("%d Years : %d Months : %d Days", years,months,days);
+
+
+
+
+// ============  program to read an amount (integer value) and break the amount into smallest possible number of bank notes.
+
+
+    
+    // int amount,a,b,c,d,e;
+    // printf("Enter A Amount : ");
+    // scanf("%d",&amount);
+    // a = amount / 2000;
+    // b = (amount % 2000) / 500;
+    // c = ((amount % 2000) % 500) / 200;
+    // d = (((amount % 2000) % 500) % 200) / 10;
+    // e = (((amount % 2000) % 500) % 200) % 10;
+    // printf("2000 Notes : %d \n500 Notes : %d \n200 Notes : %d \n10 Notes : %d \nRemaining Rupees : %d", a,b,c,d,e);
+    
+
+
+
+
+
+
+}
